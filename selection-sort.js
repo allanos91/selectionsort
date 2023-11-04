fd73a3bae168a700f39cd5b2ec2dd66d429109be
Alexander <allanosinhb@gmail.com>
2023-11-04 14:16:38 -0700

    solved with alexi

--- selection-sort.js
+++ selection-sort.js
@@ -41,29 +41,32 @@ function selectionSortInPlace(arr) {
   let pointer = 0
 
   // Repeat while the unsorted half is not empty:
-  while (pointer < arr.length)
+  while (pointer < arr.length) {
 
     // Do not move this console.log
     console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
-    let min = arr[pointer]
+    let minIndex = pointer
     for (let i = pointer; i < arr.length; i++) {
-      if (arr[i] < min) {
-        // Save the min value
-        arr[pointer] = min
-        boolean = false
+      if (arr[minIndex] > arr[i]) {
+        minIndex = i
       }
     }
-
-
+    //Save the min value
+    let minValue = arr[minIndex]
 
     // Shift every unsorted value to the left of the min value to the right by 1
-
+    for (let j = minIndex; j > pointer; j--) {
+      arr[j] = arr[j-1]
+    }
     // Put the min value at the divider
+    arr[pointer] = minValue
 
     // Increment the divider and repeat
-
+    pointer ++
+  }
+  return arr
 }
 
 
